b33f84602f4a9979684b758d36638b6bdd0f7c7a
jarekiks <88145936+jarekiks@users.noreply.github.com>
2021-09-02 16:09:51 +0200

    Add files via upload

---
+++ Zadanko1/Zadanko1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+
+public class serv {
+    public static void Main() {
+    try {
+        IPAddress ipAd = IPAddress.Parse("192.168.43.182");
+       
+        TcpListener myList=new TcpListener(ipAd,12964);
+       
+        myList.Start();        
+        
+        Socket s=myList.AcceptSocket();        
+        
+        byte[] b=new byte[1024];
+        var k=s.Receive(b);
+        List<string> slowo = new List<string>();        
+        for (int i = 0; i < k; i++)
+        {
+            slowo.Add(Convert.ToChar(b[i]).ToString());
+            
+        }
+
+        Console.Write("OK");
+        string txt = String.Join("", slowo.ToArray());
+ 
+        ASCIIEncoding asen=new ASCIIEncoding();
+        s.Send(asen.GetBytes(txt));
+
+        SqlConnection con = new SqlConnection(@"Data Source=(local);Initial Catalog=Zadanko;Integrated Security=True");
+        if (con.State == ConnectionState.Closed)
+            con.Open();
+        SqlCommand cmd = new SqlCommand(); 
+        cmd.CommandText = "Insert into Tabela(Tekst)values('" + txt + "')";
+        cmd.Connection = con;
+        cmd.ExecuteNonQuery();
+        con.Close();
+
+        s.Close();
+        myList.Stop();
+
+        Console.ReadKey();
+
+        }
+    catch (Exception e) {
+        Console.WriteLine("Error..... " + e.StackTrace);
+    }    
+    }
+    
+}
---
+++ Zadanko1/Zadanko1_Client/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+using System.Data.SqlClient;
+using System.Data;
+using System.Collections.Generic;
+
+public class clnt {
+
+    public static void Main() {
+        
+        try {
+            TcpClient tcpclnt = new TcpClient();
+                        
+            tcpclnt.Connect("192.168.43.182",12964);
+
+            Console.Write("Wpisz słowo: ");
+
+            String str=Console.ReadLine();
+            Stream stm = tcpclnt.GetStream();
+                        
+            ASCIIEncoding asen= new ASCIIEncoding();
+            byte[] ba=asen.GetBytes(str);
+                       
+            stm.Write(ba,0,ba.Length);
+            
+            byte[] bb=new byte[100];
+            int k=stm.Read(bb,0,100);
+            List<string> slowo = new List<string>();
+            for (int i=0;i<k;i++)
+                slowo.Add(Convert.ToChar(bb[i]).ToString());
+
+            string txt = String.Join("", slowo.ToArray());
+            Console.WriteLine(txt + " " + DateTime.Now.ToString("dd-MM-yyyy, HH:mm:ss"));
+
+            tcpclnt.Close();
+
+            Console.ReadKey();
+        }
+        
+        catch (Exception e) {
+            Console.WriteLine("Error..... " + e.StackTrace);
+        }
+    }
+
+}
